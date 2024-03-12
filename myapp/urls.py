451dd13cff0f27b9d76fda140118eab78e326152
Nichchaphat Sommhai <155577116+NichchaphatTukta@users.noreply.github.com>
2024-03-12 17:38:23 +0000

    4 Pages/Views

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
   path('function/', views.function_view, name='function_view'),
   path('class/', views.ClassView.as_view(), name='class_view'),
   path('theme/', views.ThemeView.as_view(), name='theme'),
+  path('profile/', views.profile_view, name='profile'),
+  path('product/', views.product_view, name='product'),
+  path('specialmenu/', views.specialmenu_view, name='specialmenu'),
+  path('ourplum/', views.ourplum_view, name='ourplum'),
 ]
